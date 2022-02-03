3816251e3f6d86e0c1f3a7748844db702ce611c6
Quibly <trippy524@yahoo.com>
2022-02-03 14:53:41 -0700

    populated director.py and parachute.py classes

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,12 +1,61 @@
+from game.answer import answer
+from game.guesser import guesser
+from game.parachute import parachute
 
 
-class Director:
+class director:
     """The main class of the game.
 
     This class oversees the organization of the gameplay.
 
     Attributes:
-        TODO
+        
     
     """
-    pass
+    def __init__(self):
+        """
+        
+        Args:
+        """
+        self._answer = answer()
+        self._current_guess = ''
+        self._guesser = guesser()
+        self._parachute = parachute()
+
+
+    def start_game(self):
+        """Method for structuring the order of events of game play
+        """
+        self._parachute._create_parachute()
+        while self._parachute._parachute_exists:
+            self._get_answer()
+            self._get_parachute()
+            self._get_guesser()
+            self._end_game()
+
+    def _get_answer(self):
+        """
+        """
+        pass
+
+    def _get_parachute(self):
+        """
+        """
+        if self._parachute._parachute_exists:
+            self._parachute._display_parachute
+
+
+
+
+    def _get_guesser(self):
+        """
+        """
+        pass
+    
+
+    def _end_game(self):
+        """Method for end game display
+        """
+        if not(self._parachute._parachute_exists()):
+            print(f'\nYour parachute is gone. Your game is over.')
+
---
+++ jumper/game/parachute.py
@@ -0,0 +1,37 @@
+from game.director import director
+
+class parachute:
+    """This class handles the display and parachute condition tracking.
+
+    Attributes:
+    
+    """
+
+    def __init__(self):
+        """
+        
+        Args:
+        """
+        self._parachute = []
+        self._parachute_exists = True
+        self._parachute_counter = int(7)
+
+
+    def _create_parachute(self):
+        """
+        """
+        self._parachute = [' / \\ ', ' /|\\ ', '  O  ', ' \\ / ', '\\   /', '/___\\', ' ___ ']
+
+    
+    def _display_parachute(self):
+        """
+        """
+        for i in range(len(self._parachute), 0, -1):
+            print(f' {self._parachute[i]}')
+
+        print('\n^^^^^^^\n')
+
+    def _parachute_exists(self):
+        """
+        """
+        return not(self._parachute)
