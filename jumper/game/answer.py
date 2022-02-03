0dd6f480d34d5762fa7ccb350b34a30b5ce00f8d
jfunky11 <82351460+jfunky11@users.noreply.github.com>
2022-02-03 13:00:57 -0800

    updated answe.py

---
+++ jumper/game/answer.py
@@ -0,0 +1,21 @@
+import random
+from guesser import guesser
+
+class answer:
+
+    def __init__(self):
+        WORDS = ("python", "jumble", "easy", "difficult", "answer",  "thursday")
+        self._word = random.choice(WORDS)
+
+    def get_word(self):
+        return self._word
+
+    def move_word(self, word):
+        self._word = word
+
+    def answer_display(self, word):
+        word_count = len(word)
+        for i in range(word_count):
+            print("_")
+
+        
