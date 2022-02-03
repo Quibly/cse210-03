3017ab0c1d25126465e965e4198cb93b47539334
Quibly <trippy524@yahoo.com>
2022-02-03 15:50:46 -0700

    updated director and guesser classes

--- jumper/game/guesser.py
+++ jumper/game/guesser.py
@@ -1,4 +1,33 @@
+from game.answer import answer
 
 class guesser:
-    def _get_inputs(self):
-        guess = self._read_letter("\nEnter a letter: ")
+
+    """
+    Class for getting input from the user and checking whether the guess is found
+    in the answer.
+
+    Attributes
+
+    """
+    def __init__(self):
+        """
+        """
+        self._guess = ''
+        self._answer = answer()
+        self._word = ''
+
+    def _get_letter(self):
+        """
+        """
+        self.guess = input("\nPlease guess a letter [a-z]: ")
+
+    def _letter_is(self):
+        """
+        """
+        return self.guess
+
+    def _letter_in_word(self):
+        """
+        """
+        self._answer.get_word()
+        return (self.guess in self._answer.get_word.split(''))
