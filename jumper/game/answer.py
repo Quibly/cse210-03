32d056c49ff2d45b0e425a4f53dc5f0c6c57267f
jfunky11 <82351460+jfunky11@users.noreply.github.com>
2022-02-03 13:56:13 -0800

    updates

--- jumper/game/answer.py
+++ jumper/game/answer.py
@@ -3,6 +3,7 @@ from guesser import guesser
 
 class answer:
 
+
     def __init__(self):
         WORDS = ("python", "jumble", "easy", "difficult", "answer",  "thursday")
         self._word = random.choice(WORDS)
@@ -14,7 +15,21 @@ class answer:
         self._word = word
 
     def answer_display(self, word, new_letter):
-        word_count = "_" * len(word)
+        guessed = False
+        while not guessed:
+            word_count = "_ " * len(word)
+            print(word_count)
+            print(word)
+            guess = input("Please guess a letter or word: ")
+            word_as_list = list(word_count)
+            indices = [i for i, letter in enumerate(word) if letter == guess]
+            for index in indices:
+                word_as_list[index] = guess
+                word_count = "".join(word_as_list)
+                if "_" not in word_count:
+                    guessed = True
+    
+
         print(word_count)
         
 
--- jumper/game/guesser.py
+++ jumper/game/guesser.py
@@ -1,4 +1,4 @@
 
 class guesser:
     def _get_inputs(self):
-        new_letter = self._read_letter("\nEnter a letter: ")
+        guess = self._read_letter("\nEnter a letter: ")
